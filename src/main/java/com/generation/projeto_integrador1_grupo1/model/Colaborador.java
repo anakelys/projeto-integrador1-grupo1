a6cea44dc53ea33729356fad3606a12e31ccb4a1
anakelys <souzanakely@gmail.com>
2025-03-07 10:42:28 -0300

    Model

--- src/main/java/com/generation/projeto_integrador1_grupo1/model/Colaborador.java
+++ src/main/java/com/generation/projeto_integrador1_grupo1/model/Colaborador.java
@@ -1,5 +1,72 @@
 package com.generation.projeto_integrador1_grupo1.model;
 
-public class Colaborador {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
+@Entity
+@Table (name = "tb_colaboradores")
+public class Colaborador {
+	
+	@Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank (message = "O atributo nome é obrigatorio")// NOT NULL 
+	private String nome;
+	
+	@NotBlank (message = "O atributo cargo é obrigatorio")
+	private String cargo;
+	
+	@NotBlank (message = "O atributo departamento é obrigatorio")
+	private String departamento;
+	
+	@NotNull
+	private double salario;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	
 }
